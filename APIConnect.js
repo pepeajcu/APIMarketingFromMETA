d22f78a83e838919c3462999332ceab4b5bb2dbc
José Ajcú <josemiguelajcu@gmail.com>
2023-03-09 14:29:14 -0600

    Comentarios

--- APIConnect.js
+++ APIConnect.js
@@ -1,21 +1,26 @@
 function importMetaMarketingAdsData() {
 
+  // URL de META para poder llamar a la API
   var apiUrl = "https://graph.facebook.com";
   
+  // Versión de la API
   var verAPI = "v16.0"
 
-  var cuenta = "1522094337894169" //act_1522094337894169/
+  // ID de la cuenta publicitaria a consultar los datos
+  var cuenta = "1522094337894169" 
 
+  // Filtros y otros parametros (pendiente de detallar)
   var filter = "insights?fields=account_id,campaign_id,campaign_name,clicks,cpc,cpm,frequency,impressions,reach,spend,date_start,date_stop&level=campaign&limit=1000&date_preset=last_month&time_increment=all_days"
 
+  // API Key Pública
   var apiKey = "EAAOgcbfZC1QwBAAMct7kxDY3idBcHZAJpO8f6Fn9pszAAn1fiotYWaGNhYWVIvFIEdUn4knayYP0G0668HUc51J3zscd7KnOcaL23wk7NvEpkszwO6eJhrF2oZCN2fe30MBwMgvVn63FN6su1ANaayZA0BgvtVLcBAcY4qfcKVLjxHwWGou0MnZCUztpbe0RyN6XvcX8d1LIjQSFfjjjF"
 
- var URL = apiUrl + "/" + verAPI + "/" + "act_" + cuenta + "/" + filter + "&access_token=" apikey
+  var apiURLMETA = apiUrl + "/" + verAPI + "/" + "act_" + cuenta + "/" + filter + "&access_token=" apikey
 
   var spreadsheetId = "14rbdS09bHKGtv3wIbxWLiPk_HjxrZ2FoQDdIs369G3g";
   var sheetName = "Hoja1";
 
-  var response = UrlFetchApp.fetch(apiUrl);
+  var response = UrlFetchApp.fetch(apiURLMETA);
 
   // Parse the API response and write the data to the Google Sheet
   var data = JSON.parse(response.getContentText());
